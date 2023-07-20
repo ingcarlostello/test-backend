f8fcc2af0d5bd2bcc2c2f2cd047e1e7904e9b282
Carlos Tello <ingcarlostello@hotmail.com>
2023-07-20 08:22:00 -0500

    chore: performance optimization code

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,37 +1,77 @@
+// @helpers
+import { formatNumberToCurrency } from "../helpers/formatNumberToCurrency.js";
+import { getCategoryPath } from "../helpers/getCategoryPath.js";
+
 const getMercadoLibreItems = async (req, res) => {
     const query = req.query.q;
 
-    const response = await fetch(
-        `https://api.mercadolibre.com/sites/MLA/search?q=${query}`
-    );
-
-    const results = await response.json();
-
-
-    const formattedResults = {
-        author: {
-            name: 'Carlos',
-            lastname: 'Tello',
-          },
-          categories:results.filters,
-          items:results.results.map((result) => (
-            {
+    try {
+        const response = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`);
+        const results = await response.json();
+        const breadCrumb = getCategoryPath(results);
+        const formattedResults = {
+            author: {
+                name: "Carlos",
+                lastname: "Tello",
+            },
+            categories: breadCrumb,
+            items: results.results.slice(0, 4).map((result) => ({
                 id: result.id,
                 title: result.title,
                 price: {
                     currency: result.currency_id,
-                    amount: result.installments.amount,
+                    amount: formatNumberToCurrency(result.price),
                 },
                 picture: result.thumbnail,
                 condition: result.condition,
-                free_shipping: result.shipping.free_shipping
-            }
-          ))
-
+                free_shipping: result.shipping.free_shipping,
+            })),
+        };
+        res.json(formattedResults);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    res.json(formattedResults);
-
 };
 
-export { getMercadoLibreItems };
+const getProductDetails = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const [responseProductDetails, responseProductDescription] =
+            await Promise.all([
+                fetch(`https://api.mercadolibre.com/items/${id}`),
+                fetch(`https://api.mercadolibre.com/items/${id}/description`),
+            ]);
+
+        const [resultsProductDetails, resultsProductDescription] =
+            await Promise.all([
+                responseProductDetails.json(),
+                responseProductDescription.json(),
+            ]);
+
+        const formattedResults = {
+            author: {
+                name: "Carlos",
+                lastname: "Tello",
+            },
+            item: {
+                id: resultsProductDetails.id,
+                title: resultsProductDetails.title,
+                price: {
+                    currency: resultsProductDetails.currency_id,
+                    amount: formatNumberToCurrency(resultsProductDetails.price),
+                },
+                picture: resultsProductDetails.pictures[0].secure_url,
+                condition: resultsProductDetails.condition,
+                free_shipping: resultsProductDetails.shipping.free_shipping,
+                sold_quantity: resultsProductDetails.sold_quantity,
+                description: resultsProductDescription.plain_text,
+            },
+        };
+
+        res.json(formattedResults);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch products" });
+    }
+};
+
+export { getMercadoLibreItems, getProductDetails };
